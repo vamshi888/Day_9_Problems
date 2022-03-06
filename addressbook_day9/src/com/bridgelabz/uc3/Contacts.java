8b88408a1c90dae5234961a1277ac7b386585df1
vamshi <vamshik484@gmail.c0m>
2022-03-06 10:40:37 +0530

    uc3

---
+++ addressbook_day9/src/com/bridgelabz/uc3/Contacts.java
@@ -0,0 +1,62 @@
+package com.bridgelabz.uc3;
+
+public class Contacts {
+    private String firstName;										
+    //Variable Declaration for Contacts Class
+    private String lastname;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNo;
+    private String emailID;
+public String getFirstName() {									  
+	//Adding Getter and Setter Methods for the Variables
+    return firstName;
+}
+public void setFirstName(String firstName) {
+    this.firstName = firstName;
+}
+public String getLastname() {
+    return lastname;
+}
+public void setLastname(String lastname) {
+    this.lastname = lastname;
+}
+public String getAddress() {
+    return address;
+}
+public void setAddress(String address) {
+    this.address = address;
+}
+public String getCity() {
+    return city;
+}
+public void setCity(String city) {
+    this.city = city;
+}
+public String getState() {
+    return state;
+}
+public void setState(String state) {
+    this.state = state;
+}
+public String getZip() {
+    return zip;
+}
+public void setZip(String zip) {
+    this.zip = zip;
+}
+public String getPhoneNo() {
+    return phoneNo;
+}
+public void setPhoneNo(String phoneNo) {
+    this.phoneNo = phoneNo;
+}
+public String getEmailID() {
+    return emailID;
+}
+public void setEmailID(String emailID) {
+    this.emailID = emailID;
+}
+}
